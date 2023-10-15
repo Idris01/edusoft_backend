a005a2a5952d6446955fab590bb8fb7aaa37404a
Idris Adebowale <idrys01@gmail.com>
2023-10-15 18:53:47 +0000

    add Tuition model

--- backend/models.py
+++ backend/models.py
@@ -18,15 +18,45 @@ class University(BaseModel):
     country = CountryField()
     city = models.CharField(max_length=50, null=False)
     postal_code = models.CharField(max_length=15, null=False, blank=False)
+    accomodation = models.TextField(help_text="details of accomodation")
+
 
     language = models.CharField(max_length=15, default='English', null=False)
 
     def __str__(self):
         return self.name
 
+class Department(BaseModel):
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    about = models.TextField(help_text="brief information about the department/school")
+    
+    def __str__(self):
+        return f"{self.name} in {self.university.name}"
+
 class Course(BaseModel):
-    university_id = models.ForeignKey(University, null=False, on_delete=models.CASCADE)
-    degree = models.ForeignKey('Degree', blank=False, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, null=False, on_delete=models.CASCADE)
+    about = models.TextField(help_text="course description")
+
+    def __str__(self):
+        return "{} in {}".format(
+                self.department,
+                self.department.univerity)
+
+class Tuition(BaseModel):
+    degree = models.ForeignKey('Degree', on_delete=models.CASCADE)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=9, decimal_places=2, help_text="tuition fee in dollars ($)")
+    detail = models.TextField(help_text="details info about the tuition fee")
+
+    def __str__(self):
+        return "${} for {} in {}".format(
+                self.amount, self.degree.name,
+                self.course.name)
+
 
 class Degree(BaseModel):
-    pass
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    about = models.TextField(help_text="breif details about degree")
+
+    def __str__(self):
+        return self.name
