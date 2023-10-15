5e298ccdffc9f73e5233ca6f14f6ab9b6d3b6a2e
Idris Adebowale <idrys01@gmail.com>
2023-10-15 12:41:55 +0100

    Setup template for admin site

---
+++ backend/models.py
@@ -0,0 +1,30 @@
+from django_countries.fields import CountryField
+from django.db import models
+import uuid
+
+# Create your models here.
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=250, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+
+class University(BaseModel):
+    website = models.URLField(null=False, blank=False)
+    history = models.TextField()
+    country = CountryField()
+    city = models.CharField(max_length=50, null=False)
+    postal_code = models.CharField(max_length=15, null=False, blank=False)
+
+    language = models.CharField(default='English', null=False)
+
+    def __str__(self):
+        return self.name
+
+class Course(BaseModel):
+    university_id = models.ForeignKey(University, null=False, on_delete=models.CASCADE)
+    degree = models.ForeignKey('Degree', blank=False, on_delete=models.CASCADE)
+
