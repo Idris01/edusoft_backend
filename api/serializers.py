b5130c060418ba9359808c5c6305107de19cd08c
Idris Adebowale <idrys01@gmail.com>
2023-10-25 11:10:25 +0000

    update University Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,34 @@ from backend.models import University
 
 
 class UniversitySerializer(serializers.ModelSerializer):
+    city = serializers.SerializerMethodField()
+    country = serializers.SerializerMethodField()
+    languages = serializers.SerializerMethodField()
+    created_by = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = University
-        fields = '__all__'
+        fields = [
+            "id",
+            "name",
+            "history",
+            "languages",
+            "created_by",
+            "country",
+            "city",
+            "accomodation",
+            "website",
+        ]
+
+    def get_languages(self, obj):
+        languages = [item.name for item in obj.languages.all()]
+        return languages
+
+    def get_created_by(self, obj):
+        return obj.created_by.username
+
+    def get_city(self, obj):
+        return obj.city.name
+
+    def get_country(self, obj):
+        return obj.country.name
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import UniversityListCreateAPIView
+
 app_name = "api"
 
 urlpatterns = [
-        path("", UniversityListCreateAPIView.as_view(), name="university_list_create")
-        ]
+    path(
+        "universities/",
+        UniversityListCreateAPIView.as_view(),
+        name="university_list_create",
+    )
+]
